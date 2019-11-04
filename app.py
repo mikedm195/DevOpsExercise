578143afec6cae0729227ea60937c96e4ad93078
Miguel <miguel.delmoral@jonajo.com>
2019-11-04 16:51:55 -0600

    Set get method and set status codes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask.logging import create_logger
 from pymongo import MongoClient
 from flasgger import Swagger
 import os
 import logging
+import json
 
 app = Flask(__name__)
 swagger = Swagger(app)
@@ -12,37 +13,24 @@ logger = create_logger(app)
 logger.setLevel(logging.INFO)
 
 
-@app.route('/search',  methods=['POST'])
+@app.route('/search')
 def search():
     """Example endpoint returning a list of colors by palette
     This is using docstrings for specifications.
     ---
     parameters:
-      - in: body
+      - in: query
         name: name
         required: true
-        description: Complete or part of name
-        schema:
-          $ref: '#/definitions/Request'
+        description: Name of the book
+        default: Android
     definitions:
       Book:
         type: object
         properties:
-          _id:
-            type: string
           title:
             type: string
-          isbn:
-            type: string
-          pageCount:
-            type: string
-          publishedDate:
-            type: string
           shortDescription:
-            type: string
-          status:
-            type: string
-          thumbnailUrl:
             type: string
           authors:
             type: array
@@ -56,14 +44,9 @@ def search():
         type: string
       Category:
         type: string
-      Request:
-        type: object
-        properties:
-          name:
-            type: string
     responses:
       200:
-        description: A list of colors (may be filtered by palette)
+        description: A book
         schema:
           $ref: '#/definitions/Book'
         examples:
@@ -78,31 +61,24 @@ def search():
                 "Open Source",
                 "Mobile"
             ],
-            "isbn": "1933988673",
-            "pageCount": 416,
-            "publishedDate": "Wed, 01 Apr 2009 07:00:00 GMT",
-            "shortDescription": "Unlocking Android: A Developer's Guide provides concise, hands-on instruction for the Android operating system and development tools. This book teaches important architectural concepts in a straightforward writing style and builds on this with practical and useful examples throughout.",
-            "status": "PUBLISH",
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
+            "shortDescription": "Unlocking Android: A Developer's Guide provides concise, hands-on instruction for the Android operating system and development tools. This book teaches important architectural concepts in a straightforward writing style and builds on this with practical and useful examples throughout.",            
             "title": "Unlocking Android"
           }
     """
-    book_name = request.json["name"]
-    connection = MongoClient(
-        os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
-    db = connection.get_database('sample_library')
-    books_collection = db.get_collection('books')
-    books = books_collection.find_one({ "title": {'$regex': book_name}},{"longDescription":0})
-
-    return jsonify(books)
-
-
-@app.before_first_request
-def initialize():
-    connection = MongoClient(
-        os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
-    db = connection.get_database('sample_library')
-    books_collection = db.get_collection('books')
+    try:
+        book_name = request.args.get("name")
+        connection = MongoClient(
+            os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
+        db = connection.get_database('sample_library')
+        books_collection = db.get_collection('books')
+        books = books_collection.find_one({ "title": {'$regex': book_name}},{"title":1, "shortDescription": 1, "authors": 1, "categories": 1})
+        if books is not None:
+            return Response(json.dumps(books), status=200, mimetype='application/json')
+        else:
+            return Response(json.dumps({"Error": "The book could not be found"}), status=404, mimetype='application/json')
+    except Exception as e:
+        return Response(json.dumps({"Error": str(e)}), status=500, mimetype='application/json')
+    
 
 
 if __name__ == '__main__':
